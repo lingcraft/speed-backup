71bb969ed3091b4e8a5db24b0d11a63d15e8d025
lingcraft <lingcraft@qq.com>
2023-09-19 15:23:43 +0800

    test

--- convert.py
+++ convert.py
@@ -1,9 +1,16 @@
-import os, zhconv
+import os
+import zhconv
 
-for root, dirs, files in os.walk("backup_script"):
-    for file_name in list(filter(lambda e: e.endswith((".sh", ".conf")) or root.endswith("script"), files)):
-        with open(os.path.join(root, file_name), "r", encoding="utf-8") as f:
-            content = f.read()
-        os.remove(os.path.join(root, file_name))
-        with open(os.path.join(root, zhconv.convert(file_name, 'zh-cn')), "w", encoding="utf-8", newline="\n") as f:
-            f.write(zhconv.convert(content, "zh-cn"))
+
+def main():
+    for root, dirs, files in os.walk("backup_script"):
+        for file_name in list(filter(lambda e: e.endswith((".sh", ".conf")) or root.endswith("script"), files)):
+            with open(os.path.join(root, file_name), "r", encoding="utf-8") as f:
+                content = f.read()
+            os.remove(os.path.join(root, file_name))
+            with open(os.path.join(root, zhconv.convert(file_name, 'zh-cn')), "w", encoding="utf-8", newline="\n") as f:
+                f.write(zhconv.convert(content, "zh-cn"))
+
+
+if __name__ == "__main__":
+    main()

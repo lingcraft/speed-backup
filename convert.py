65d5203b8baf2a84fe031360f561ca9891ab101c
lingcraft <lingcraft@qq.com>
2025-12-25 11:51:22 +0800

    使用更好用的opencc库进行繁体转换，修正部分未转换的名词

--- convert.py
+++ convert.py
@@ -1,22 +1,32 @@
-import github, os, shutil, zhconv
+import github, os, shutil, opencc
 
 auth = github.Auth.Token(os.getenv("GITHUB_TOKEN"))
 git = github.Github(auth=auth)
 proj_name = "speed-backup"
 my_repo = git.get_repo(f"lingcraft/{proj_name}")
 src_repo = git.get_repo("YAWAsau/backup_script")
+converter = opencc.OpenCC("tw2sp")
 trans_dict = {
     # 脚本替换内容
     "shell_language=\"zh-TW\"": "shell_language=\"zh-CN\"",
-    "萤幕": "屏幕",
-    "备分": "备份",
-    "使用者": "用户",
     "安装档": "安装包",
     "遗失": "丢失",
-    # readme.md 替换内容
+    "重新开机套用": "重启生效",
+    "过世": "寄",
+    "lock 档": "lock 文件",
+    "文档": "文件",
+    "单档": "单文件",
+    "压缩档": "压缩文件",
+    "损毁": "损坏",
+    "意外断开": "连接断开",
+    "尽速": "立即",
+    "提升": "升级",
+    "画面回报": "并反馈给",
+    "杀死": "关闭",
+    "结束自身": "关闭自己",
+    # README 替换内容
     "数据备份脚本": "数据备份脚本【简体中文版】",
     "安桌": "安卓",
-    "支援": "支持",
     "backup_script.zip": "speed-backup.zip",
     "QQ组": "QQ群",
     "铭谢贡献": "感谢贡献者"
@@ -36,9 +46,9 @@ def get_latest_release():
     global version, description
     release = src_repo.get_latest_release()
     if len(release.tag_name):
-        for asset in release.assets:
-            asset.download_asset(asset.name, 8192)
-            shutil.unpack_archive(asset.name, proj_name)
+        asset = release.get_assets()[0]
+        asset.download_asset(asset.name, 8192)
+        shutil.unpack_archive(asset.name, proj_name)
         if os.path.isdir(proj_name):
             version, description = release.tag_name, convert(release.body)
             return True
@@ -46,7 +56,7 @@ def get_latest_release():
 
 
 def convert(content):
-    content = zhconv.convert(content, "zh-cn")
+    content = converter.convert(content)
     for key, value in trans_dict.items():
         content = content.replace(key, value)
     return content
@@ -68,13 +78,16 @@ def upload():
     release = next((release for release in my_repo.get_releases() if release.tag_name == version), None)
     if release is None:
         release = my_repo.create_git_release(version, version, description, False, False, False)
-        release.upload_asset(f"{proj_name}.zip", f"{proj_name}{version}.zip", "application/zip", f"{proj_name}{version}.zip")
     else:
-        for asset in release.get_assets():
+        assets = release.get_assets()
+        if assets.totalCount > 0:
+            asset = assets[0]
             asset.download_asset(asset.name, 8192)
             if os.path.getsize(f"{proj_name}.zip") != os.path.getsize(asset.name):
                 asset.delete_asset()
-                release.upload_asset(f"{proj_name}.zip", f"{proj_name}{version}.zip", "application/zip", f"{proj_name}{version}.zip")
+            else:
+                return
+    release.upload_asset(f"{proj_name}.zip", f"{proj_name}{version}.zip", "application/zip", f"{proj_name}{version}.zip")
 
 
 def update_readme():
